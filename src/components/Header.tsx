5f64346ecc336882556ea9ccda6d1cbbc101df67
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-14 06:45:07 +0100

    Refatore: Minus code in page add padding in page and change header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,17 +9,16 @@ export function Header() {
   const date = dayjs(Date.now()).format('DD / MM / YYYY')
 
   return (
-    <div className="flex min-w-full justify-center items-center relative p-4">
+    <div className="flex w-full justify-center items-center relative">
       <header className="flex items-center justify-center gap-5 font-medium">
 
         <span>{date}</span>
 
-
-        <nav className="py-4 px-5 rounded-full bg-neutral-950 flex items-center justify-center gap-4 text-neutral-50">
+        <nav className="py-4 px-5 rounded-full bg-neutral-950 dark:bg-neutral-50 flex items-center justify-center gap-4 text-neutral-50 dark:text-neutral-950">
           <li className="list-none p-2 hover:bg-neutral-800/70 rounded-full hover:scale-125 transition"><Home size={24} /></li>
           <li className="list-none p-2 hover:bg-neutral-800/70 rounded-full hover:scale-125 transition"><FolderOpen size={24} /></li>
           <li className="list-none">
-            <Avatar className="bg-neutral-50 rounded-full text-neutral-950 h-10 w-10">
+            <Avatar className="rounded-full h-10 w-10">
               <AvatarImage
                 src="https://github.com/clemilsonazevedo.png"
                 alt="@ClemilsonAzevedo" />
@@ -30,11 +29,11 @@ export function Header() {
           <li className="list-none p-2 hover:bg-neutral-800/70 rounded-full hover:scale-125 transition"><Github size={24} /></li>
         </nav>
 
-        <div className="transition flex-1">{hoursOfTheDay()}</div>
+        <div>{hoursOfTheDay()}</div>
 
       </header>
 
-      <div className="absolute right-4 top-7 flex items-center gap-2">
+      <div className="absolute right-4 top-6 flex items-center gap-2">
         <Switch id='themeMode' />
         <label htmlFor="themeMode"><Moon /></label>
       </div>
