0968972c5c5999ba4a7196b3401495ab6fe97374
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-14 21:49:12 +0100

    Refatore: Add links on header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,9 +15,15 @@ export function Header() {
         <span>{date}</span>
 
         <nav className="py-4 px-5 rounded-full bg-neutral-950 dark:bg-neutral-100 flex items-center justify-center gap-4 text-neutral-50 dark:text-neutral-950">
-          <li className="list-none p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "><Home size={24} /></li>
-          <li className="list-none p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "><FolderOpen size={24} /></li>
-          <li className="list-none">
+          <a href="#home" target="_self" className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 ">
+            <Home size={24} />
+          </a>
+
+          <a href="#projects" target="_self" className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 ">
+            <FolderOpen size={24} />
+          </a>
+
+          <a href="#">
             <Avatar className="rounded-full h-10 w-10 p-1 bg-primary-foreground text-foreground font-bold">
               <AvatarImage
                 src="https://github.com/clemilsonazevedo.png"
@@ -26,13 +32,18 @@ export function Header() {
               />
               <AvatarFallback>C.A</AvatarFallback>
             </Avatar>
-          </li>
-          <li className="list-none p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "><X size={24} /></li>
-          <li className="list-none p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "><Github size={24} /></li>
+          </a>
+
+          <a href="https://twitter.com/_ClemAzevedo_" target="_blank" className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 ">
+            <X size={24} />
+          </a>
+
+          <a href="https://github.com/clemilsonazevedo" target="_blank" className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 ">
+            <Github size={24} />
+          </a>
         </nav>
 
         <div className="w-[63px]">{hoursOfTheDay()}</div>
-
       </header>
       <SwitchThemeMode />
     </div>
