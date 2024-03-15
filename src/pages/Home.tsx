cbf1e6a6393fe814fa2e25b8418bf9a132312a60
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-15 14:09:56 +0100

    Feat: Horizontal smoth scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,24 +3,45 @@ import { Project } from "@/components/Project";
 export function Home() {
 
   return (
-    <section className="overflow-y-scroll h-full w-[650px] flex flex-col items-center scroll-smooth justify-center text-neutral-950 dark:text-neutral-200 z-20">
+    <div className="relative h-full w-full grid items-center overflow-x-hidden scroll-smooth ">
+      <main className="text-neutral-950 dark:text-neutral-200 flex absolute gap-5 ">
 
-      <Project
-        ProjectName="Dany Cakes"
-        ProjectType="Mobile Design"
-        ProjectDoneDate={2024}
-        ProjectDesign="#"
-        ProjectRepository=""
-      />
-      <Project
-        ProjectName="Login Template"
-        ProjectType="Desktop Design"
-        ProjectDoneDate={2020}
-        ProjectDesign="#"
-        ProjectRepository="#"
-      />
+        <section id="home" className="w-screen z-20  snap-x snap-end">
+          <Project
+            ProjectName="Dany Cakes"
+            ProjectType="Mobile Design"
+            ProjectDoneDate={2024}
+            ProjectDesign="#"
+            ProjectRepository=""
+          />
+          <Project
+            ProjectName="Login Template"
+            ProjectType="Desktop Design"
+            ProjectDoneDate={2020}
+            ProjectDesign="#"
+            ProjectRepository="#"
+          />
+        </section>
 
 
-    </section>
+        <section id="projects" className="z-20 w-screen snap-x snap-end">
+          <Project
+            ProjectName="Dany Cakes"
+            ProjectType="Mobile Design"
+            ProjectDoneDate={2024}
+            ProjectDesign="#"
+            ProjectRepository=""
+          />
+          <Project
+            ProjectName="Login Template"
+            ProjectType="Desktop Design"
+            ProjectDoneDate={2020}
+            ProjectDesign="#"
+            ProjectRepository="#"
+          />
+        </section>
+
+      </main>
+    </div>
   )
 }
--- src/pages/ProjectsPresentation.tsx
+++
@@ -1,8 +0,0 @@
-export function ProjectsPresentation() {
-
-  return (
-    <section className="z-20 overflow-y-auto h-full w-full flex flex-col items-center justify-center scroll-smooth text-neutral-950 dark:text-neutral-200">
-
-    </section>
-  )
-}
