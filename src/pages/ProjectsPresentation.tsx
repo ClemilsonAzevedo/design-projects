af6208c1baea77c023a934bf806ec77cebdd6aca
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-15 12:31:19 +0100

    Fix: Page build

--- src/pages/ProjectsPresentation.tsx
+++ src/pages/ProjectsPresentation.tsx
@@ -1,17 +1,8 @@
-import { Project } from "@/components/Project";
-
 export function ProjectsPresentation() {
 
   return (
     <section className="z-20 overflow-y-auto h-full w-full flex flex-col items-center justify-center scroll-smooth text-neutral-950 dark:text-neutral-200">
 
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
     </section>
   )
 }
