3cfb0926d8bbfc6b31a2d54177f51ab223da55e8
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-18 10:59:18 +0100

    Feat: Change Router to Router Provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import { LineAnimateBackground } from './components/LineAnimateBackground';
 
 export function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+
+    {
+      path: "/projects",
+      element: <Projects />,
+    },
+  ]);
+
   return (
     <main className='h-full w-full overflow-y-auto'>
       <div className='flex z-20 h-full'>
-        <Router>
-          <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/projects" Component={Projects} />
-          </Routes>
-        </Router>
+        <RouterProvider router={router} />
       </div>
       <LineAnimateBackground />
     </main>
