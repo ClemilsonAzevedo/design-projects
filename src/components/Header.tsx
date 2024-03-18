060d4df2f43d048287e20048c9e657c773b9b62e
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-18 10:42:32 +0100

    test: change to a again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,24 +18,24 @@ export function Header() {
         ><Calendar className="fill-neutral-50 dark:fill-neutral-950" size={20} />{date}</span>
 
         <nav className="p-5 rounded-full bg-neutral-950 dark:bg-neutral-100 flex items-center justify-center gap-5 text-neutral-50 dark:text-neutral-950">
-          <Link
-            to="/"
+          <a
+            href="/"
             target="_self"
             className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "
           >
             <Home size={24} />
-          </Link>
+          </a>
 
-          <Link
-            to="/projects"
+          <a
+            href="/projects"
             target="_self"
             className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "
           >
             <FolderOpen size={24} />
-          </Link>
+          </a>
 
-          <Link
-            to="#"
+          <a
+            href="#"
             className="max-sm:hidden"
           >
             <Avatar className="rounded-full h-10 w-10 p-1 bg-primary-foreground text-foreground font-bold"
@@ -47,23 +47,23 @@ export function Header() {
               />
               <AvatarFallback>C.A</AvatarFallback>
             </Avatar>
-          </Link>
+          </a>
 
-          <Link
-            to="https://twitter.com/_ClemAzevedo_"
+          <a
+            href="https://twitter.com/_ClemAzevedo_"
             target="_blank"
             className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "
           >
             <X size={24} />
-          </Link>
+          </a>
 
-          <Link
-            to="https://github.com/clemilsonazevedo"
+          <a
+            href="https://github.com/clemilsonazevedo"
             target="_blank"
             className="p-2 hover:bg-neutral-800/70 dark:hover:bg-neutral-300/70 rounded-full hover:scale-125 "
           >
             <Github size={24} />
-          </Link>
+          </a>
         </nav>
 
         <div className="flex items-center justify-center gap-2 max-md:absolute max-md:-bottom-10 max-md:right-5">
