4538822be8cd502584fc440c820e95bce61f90ce
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-03-19 13:26:32 +0100

    Fix: local storage dark mode to fix that

--- src/components/SwitchThemeMode.tsx
+++ src/components/SwitchThemeMode.tsx
@@ -5,17 +5,45 @@ import { useState, useEffect } from 'react';
 
 export function SwitchThemeMode() {
 
-  const [theme, setTheme] = useState(<Sun />);
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      return <Moon />;
+    } else {
+      return <Sun />;
+    }
+  });
+
+  const [isDarkTheme, setIsDarkTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'dark';
+  });
 
   useEffect(() => {
     const htmlClasses = document.documentElement.classList;
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+      setTheme(<Moon />);
+      setIsDarkTheme(true);
+      htmlClasses.add('dark');
+    } else {
+      setTheme(<Sun />);
+      setIsDarkTheme(false)
+      htmlClasses.remove('dark');
+    }
+
     const observer = new MutationObserver(mutations => {
       mutations.forEach(mutation => {
         if (mutation.attributeName === 'class') {
           if (htmlClasses.contains('dark')) {
             setTheme(<Moon />);
+            setIsDarkTheme(true);
+            localStorage.setItem('theme', 'dark');
           } else {
             setTheme(<Sun />);
+            setIsDarkTheme(false);
+            localStorage.setItem('theme', 'light');
           }
         }
       });
@@ -29,9 +57,22 @@ export function SwitchThemeMode() {
     return () => observer.disconnect();
   }, []);
 
+ const handleToggleTheme = () => {
+    if (isDarkTheme) {
+      setTheme(<Sun />);
+      setIsDarkTheme(false);
+      localStorage.setItem('theme', 'light');
+    } else {
+      setTheme(<Moon />);
+      setIsDarkTheme(true);
+      localStorage.setItem('theme', 'dark');
+    }
+    toggleThemeMode();
+  };
+
   return (
     <div className="absolute flex items-center gap-2 max-md:-top-8 right-5 top-1/2 -translate-y-1/2 max-md:right-5 max-alternateLg:top-2">
-      <Switch className="dark:bg-foreground max-xl:hidden" onClick={toggleThemeMode} id="themeMode" />
+      <Switch className="dark:bg-foreground max-xl:hidden" checked={isDarkTheme} onClick={handleToggleTheme} id="themeMode" />
       <label htmlFor="themeMode" className="transition">{theme}</label>
     </div>
   )
